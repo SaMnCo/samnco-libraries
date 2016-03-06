1d565c7dc13a5500e586d5bf1d44a2a69261b46b
Samuel Cozannet <samuel.cozannet@madeden.com>
2016-03-06 17:46:50 +0100

    removing sourcing bash tbc -  document proper usage"

--- gcelib.sh
+++ gcelib.sh
@@ -23,9 +23,6 @@ FACILITY=${FACILITY:-"local0"}
 LOGTAG=${LOGTAG:-"unknown"}
 MIN_LOG_LEVEL=${MIN_LOG_LEVEL:-"debug"}
 
-BASHLIB="00_bashlib.sh"
-find ${MYDIR}/.. -name "${BASHLIB}" -exec source {} \;
-
 # Check if we are sudoer or not
 [ $(bash::lib::is_sudoer) -eq 0 ] && bash::lib::die "You must be root or sudo to run this script"
 
